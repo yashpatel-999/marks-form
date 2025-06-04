9c8adadf0d9f092b6cc990a00963b7966b408aa2
yash <thotayash47@gmail.com>
2025-06-04 15:57:57 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected title = 'marks-form';
+  name: string = '';
+  subject: string = '';
+  test1: number = 0;
+  test2: number = 0;
+  submitted: boolean = false;
+
+  submit() {
+    this.submitted = true;
+    console.log(`Name: ${this.name}, Subject: ${this.subject}, Score: ${this.test1 + this.test2}`);
+  }
 }
